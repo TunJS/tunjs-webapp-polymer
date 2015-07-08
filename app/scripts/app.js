96660f05ada5bda1e420d8c8fa845e1ef204d78f
Nader Toukabri <nader.toukabri@gmail.com>
2015-07-08 23:12:40 +0100

    added google-plus-activity, meetup-activity and meetup-members custom elements added required polyfills conditionnely

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,7 +7,11 @@ Code distributed by Google as part of the polymer project is also
 subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
 */
 
-(function(document) {
+window.MEETUP_KEY = '5c4b1d264f3925541e45676d73106f1a';
+
+window.GOOGLE_PLUS_KEY = 'AIzaSyDIeycbdMumGfVW36NPu5r-DgmFQBPxGnM';
+
+(function (document) {
   'use strict';
 
   // Grab a reference to our auto-binding template
@@ -15,27 +19,49 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
   // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
   var app = document.querySelector('#app');
 
-  app.displayInstalledToast = function() {
+  app.displayInstalledToast = function () {
     document.querySelector('#caching-complete').show();
   };
 
   // Listen for template bound event to know when bindings
   // have resolved and content has been stamped to the page
-  app.addEventListener('dom-change', function() {
+  app.addEventListener('dom-change', function () {
     console.log('Our app is ready to rock!');
   });
 
   // See https://github.com/Polymer/polymer/issues/1381
-  window.addEventListener('WebComponentsReady', function() {
+  window.addEventListener('WebComponentsReady', function () {
     // imports are loaded and elements have been registered
   });
 
   // Close drawer after menu item is selected if drawerPanel is narrow
-  app.onMenuSelect = function() {
+  app.onMenuSelect = function () {
     var drawerPanel = document.querySelector('#paperDrawerPanel');
-    if (drawerPanel.narrow) {
+    if (drawerPanel && drawerPanel.narrow) {
       drawerPanel.closeDrawer();
     }
+  };
+
+  fetch.utils = {};
+
+  fetch.utils.parseJSON = function parseJSON(response) {
+    return response.json();
+  };
+
+  fetch.utils.checkStatus = function checkStatus(response) {
+    if (response.status >= 200 && response.status < 300) {
+      return response;
+    } else {
+      var error = new Error(response.statusText);
+      error.response = response;
+      throw error;
+    }
+  };
+
+  fetch.utils.queryParams = function queryParams(params) {
+    return '?' + Object.keys(params).map(function (key) {
+      return encodeURIComponent(key) + '=' + encodeURIComponent(params[key]);
+    }).join('&');
   };
 
 })(document);
